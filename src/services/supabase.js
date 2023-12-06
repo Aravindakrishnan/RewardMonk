c492dc584460f9ab4efa1785ad8f59b8f2b44213
akrishnanh <akrishnanh@presidio.com>
2023-12-06 23:35:51 +0530

    Removed supabase secrets

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,3 +1,3 @@
 import { createClient } from "@supabase/supabase-js";
-const supabase = createClient("https://umrjofvdjihonczfbxhh.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVtcmpvZnZkamlob25jemZieGhoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDE4NzkzNDAsImV4cCI6MjAxNzQ1NTM0MH0.SSABvSXS9DlP_YjI_PTy6hrEy0cwwct5pBWfQ8aaSdg");
+const supabase = createClient("https://xxxx.supabase.co", "xxxx");
 export default supabase;
